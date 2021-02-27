b76220544dcdb613e1a3402c772c5ec4a8854e22
Hunner4D <h.herrera.academics@gmail.com>
2021-02-26 23:05:50 -0800

    sidebar

--- src/react/layout/sidebar/index.js
+++ src/react/layout/sidebar/index.js
@@ -1,7 +1,47 @@
+import sprite from "images/symbol-defs.svg";
+
 export default function Sidebar() {
   return (
     <nav className="sidebar">
-      Sidebar
+      <ul className="side-nav">
+        <li className="side-nav__item">
+          <a href="" className="side-nav__link">
+            <svg className="side-nav__icon">
+              <use xlinkHref={`${sprite}#icon-home`} />
+            </svg>
+            <span>Hotel</span>
+          </a>
+        </li>
+
+        <li className="side-nav__item">
+          <a href="" className="side-nav__link">
+            <svg className="side-nav__icon">
+              <use xlinkHref={`${sprite}#icon-aircraft-take-off`} />
+            </svg>
+            <span>Flight</span>
+          </a>
+        </li>
+
+        <li className="side-nav__item">
+          <a href="" className="side-nav__link">
+            <svg className="side-nav__icon">
+              <use xlinkHref={`${sprite}#icon-key`} />
+            </svg>
+            <span>Car Rentals</span>
+          </a>
+        </li>
+
+        <li className="side-nav__item">
+          <a href="" className="side-nav__link">
+            <svg className="side-nav__icon">
+              <use xlinkHref={`${sprite}#icon-map`} />
+            </svg>
+            <span>Attractions</span>
+          </a>
+        </li>
+      </ul>
+
+      <div className="legal">&copy; 2021 by airbrb. All rights reserved.</div>
     </nav>
   );
 }

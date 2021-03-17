1de47a7dd98369f1c7b32b56cdbe5914ef184419
Hunner4D <h.herrera.academics@gmail.com>
2021-03-16 17:12:44 -0700

    user nav dropdown

--- src/react/components/user-nav/index.js
+++ src/react/components/user-nav/index.js
@@ -2,24 +2,38 @@ import sprite from "images/symbol-defs.svg";
 import mockUser from "images/user-1.jpg";
 
 export default function UserNav() {
-    return (
-        <nav className="user-nav">
-        <div className="user-nav__icon-box">
-          <svg className="user-nav__icon">
-            <use xlinkHref={`${sprite}#icon-bookmark`} />
-          </svg>
-          <span className="user-nav__icon__notification">2</span>
+  return (
+    <nav className="user-nav">
+      <div className="user-nav__icon-box">
+        <svg className="user-nav__icon">
+          <use xlinkHref={`${sprite}#icon-bookmark`} />
+        </svg>
+        <span className="user-nav__icon__notification">2</span>
+      </div>
+      <div className="user-nav__icon-box">
+        <svg className="user-nav__icon">
+          <use xlinkHref={`${sprite}#icon-chat`} />
+        </svg>
+        <span className="user-nav__icon__notification">7</span>
+      </div>
+      <div className="user-nav__user">
+        <img src={mockUser} alt="User photo" className="user-nav__user-photo" />
+        <span className="user-nav__user-name">Kenny Ng</span>
+
+        <div className="user-nav__user__dropdown">
+          <ul className="user-nav__user__dropdown__list">
+            <li className="user-nav__user__dropdown__item">
+              <a className="user-nav__user__dropdown__item__link">Profile</a>
+            </li>
+            <li className="user-nav__user__dropdown__item">
+              <a className="user-nav__user__dropdown__item__link">Settings</a>
+            </li>
+            <li className="user-nav__user__dropdown__item">
+              <a className="user-nav__user__dropdown__item__link">Sign Out</a>
+            </li>
+          </ul>
         </div>
-        <div className="user-nav__icon-box">
-          <svg className="user-nav__icon">
-            <use xlinkHref={`${sprite}#icon-chat`} />
-          </svg>
-          <span className="user-nav__icon__notification">7</span>
-        </div>
-        <div className="user-nav__user">
-          <img src={mockUser} alt="User photo" className="user-nav__user-photo" />
-          <span className="user-nav__user-name">Kenny Ng</span>
-        </div>
-      </nav>
-    )
+      </div>
+    </nav>
+  );
 }
--- src/react/layout/sidebar/index.js
+++ src/react/layout/sidebar/index.js
@@ -9,7 +9,7 @@ export default function Sidebar() {
   return (
     <nav className="sidebar">
       <ul className="side-nav">
-        <li className="side-nav__item">
+        <li className="side-nav__item side-nav__item--active">
           <a href="#" className="side-nav__link">
             <svg className="side-nav__icon">
               <use xlinkHref={`${sprite}#icon-home`} />

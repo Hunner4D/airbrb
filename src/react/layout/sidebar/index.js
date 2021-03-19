7ec7ac3cfe2ca617c50a401d59b7915792bd484c
Hunner4D <h.herrera.academics@gmail.com>
2021-03-19 15:41:28 -0700

    side-nav class change

--- src/react/layout/sidebar/index.js
+++ src/react/layout/sidebar/index.js
@@ -9,7 +9,8 @@ export default function Sidebar() {
   return (
     <nav className="sidebar">
       <ul className="side-nav">
-        <li className="side-nav__item side-nav__item--active">
+        {/* <li className="side-nav__item side-nav__item--active"> */}
+        <li className="side-nav__item">
           <a href="#" className="side-nav__link">
             <svg className="side-nav__icon">
               <use xlinkHref={`${sprite}#icon-home`} />
